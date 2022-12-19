5ec5ab5cbf2e84391be1779d698cd92a4249e183
jaxongir2002 <jaxongirxon.rustamov12@gmail.com>
2022-12-19 14:48:02 +0500

    hjghgjhg

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+ <Home></Home>
+<Footer1></Footer1>
+</template>
+
+
+<script>
+
+import Home from'./components/Home.vue'
+import Footer1 from'./components/Footer.vue'
+export default{
+  components:{
+  Home,
+  Footer1
+},
+  data(){
+    return{
+
+
+    }
+  }
+}
+
+</script>
+
+
+<style scoped>
+
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,104 @@
+
+
+<template>
+
+<Navigation></Navigation>
+
+
+
+</template>
+  
+<script>
+import Navigation from './Navigation.vue'
+
+export default{
+  components:{
+    Navigation
+  }
+}
+
+</script>
+  
+<style scoped>
+.btn4 {
+  background-color: #FFBC51;
+  box-shadow: 3px 3px 10px #FFBC51;
+  transition: .3s;
+  width: 80px;
+  height: 33px;
+}
+
+.btn4:hover {
+  transform: translateY(-5px);
+}
+
+a {
+  color: white !important;
+  transition: .5s;
+  font-family: sans-serif;
+}
+
+a:hover {
+  color: rgba(0, 0, 0, 0.505) !important;
+}
+
+.btn3 {
+  border: none;
+  box-shadow: none;
+  transition: .3s;
+  width: 80px;
+  height: 33px;
+
+}
+
+.btn3:hover {
+  background-color: #f4c67d72;
+
+}
+
+li {
+  list-style: none;
+}
+
+
+svg {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.wave-header {
+  min-height: 100vh;
+  width: 100%;
+  background: linear-gradient(99.22deg, #5C0E62 1.27%, #00B5D0 92.23%);
+  position: relative;
+}
+
+.btn5 {
+  background-color:
+    rgba(121, 138, 201, 1) !important;
+  transition: .4s;
+  width: 100px;
+  height: 40px;
+}
+
+.p {
+  color: rgba(255, 255, 255, 1);
+  font-family: sans-serif;
+}
+
+.h1 {
+  font-family: sans-serif;
+  font-size: 60px !important;
+}
+
+.btn5:hover {
+  transform: translateY(-5px) !important;
+}
+</style>
