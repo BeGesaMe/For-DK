6ca95ff92d5ae59d8a4b3ab1b64764d38eda1714
jaxongir2002 <jaxongirxon.rustamov12@gmail.com>
2022-12-24 15:53:12 +0500

    dsddssfsd

--- src/main.js
+++ src/main.js
@@ -4,5 +4,17 @@ import 'mdb-vue-ui-kit/css/mdb.min.css';
 import router from './routes/router'
 import App from './App.vue'
 let app = createApp(App)
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+
+app.use(vuetify)
 app.use(router)
 app.mount('#app')
